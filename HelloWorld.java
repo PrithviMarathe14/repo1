6c796682f06be98de1ef6953bd0da96701964c5d
PrithviMarathe14 <49053140+PrithviMarathe14@users.noreply.github.com>
2021-03-23 08:46:57 +0530

    added attributes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ class HelloWorld{
 public static void main(String []args){
 System.out.println("Hello");
  add(10,20);
+ mul(5,10);
+ sub(35,25);
 }
  public static void add(int num1,int num2){
       System.out.println(num1+num2);
