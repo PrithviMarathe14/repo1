500c35a6a370c6cce720f1e490f3e269ef920740
hp <hp@LAPTOP-3FUE7UP8>
2021-03-23 08:45:53 +0530

    mul function added

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,4 +9,7 @@ System.out.println("Hello");
  public static void sub(int num1,int num2){
      System.out.println(num1-num2);
  }
+ public static void mul(int num1,int num2){
+     System.out.println(num1*num2);
+ }
 }
